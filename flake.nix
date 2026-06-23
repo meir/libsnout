2c5d54f262a365e1e8175c8761a29bd86dd04e38
meir <michaelmeir1222001@email.com>
2026-06-23 11:49:30 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Rust library for face and eye tracking based on project babble";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+  let
+    inherit (nixpkgs) lib;
+    eachSystem = function: lib.genAttrs (
+      lib.platforms.linux ++ lib.platforms.darwin
+    ) (system:
+      function (import nixpkgs {
+        inherit system;
+      })
+    );
+  in
+  {
+    packages = eachSystem (pkgs: {
+      default = pkgs.rustPlatform.buildRustPackage {
+        pname = "snout-cli";
+        version = "main";
+
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+        cargoBuildFlags = [ "--package" "snout-cli" ];
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          rustPlatform.bindgenHook
+          makeWrapper
+        ];
+
+        postFixup = let
+          libs = with pkgs; lib.makeLibraryPath [
+            llvm
+            onnxruntime
+            vulkan-loader
+          ];
+        in
+        ''
+          wrapProgram "$out/bin/snout-cli" \
+            --prefix LD_LIBRARY_PATH : "${libs}"
+        '';
+
+        meta = {
+          description = "A library for snout detection and tracking";
+          homepage = "https://github.com/Darksecond/libsnout";
+          platforms = lib.platforms.linux;
+          mainProgram = "snout-cli";
+        };
+      };
+    });
+  };
+}
